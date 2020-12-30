bb0ecde3c8d633455e273004049ec2b1a9a198f8
zzz19680410 <1737403035@qq.com>
2020-12-30 15:28:44 +0800

    修改首页样式

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,100 +1,64 @@
 <template>
 	<view class="rf-index">
-		<!--搜索导航栏-->
-		<rf-search-bar
-				@link="toCategory"
-				@search="navToSearch"
-				title="分类"
-				icon="iconfenlei1"
-				:placeholder="hotSearchDefault"/>
-		<!-- 轮播图1 -->
+		<!-- 顶部信息 -->
+		<view class="header">
+			<!--位置信息-->
+			<view class="location">
+				<view class="location_text">西安</view>
+				<view>
+					<image class="location_img" src="../../static/home/City.svg"></image>
+				</view>				
+			</view>
+			<view class="notice">
+				<image class="notice_img" src="../../static/home/Notification.svg"></image>
+			</view>
+		</view>
+		
+		<!-- 轮播图 -->
 		<view class="swiper">
 			<view class="swiper-box">
-				<rf-swipe-dot :info="carouselList.index_top" mode="nav" :current="current" field="title">
+				<!-- <rf-swipe-dot :info="carouselList.index_top" mode="nav" :current="current" field="title"> -->
 					<swiper @change="handleDotChange" autoplay="true">
-						<swiper-item
+						<!-- <swiper-item
 								v-for="(item, index) in carouselList.index_top"
 								@tap="indexTopToDetailPage(item.jump_type, item.jump_link)"
-								:key="index">
+								:key="index"> -->
+						<swiper-item>
 							<view class="swiper-item">
-								<image :src="item.cover" mode="aspectFill"/>
+								<!-- <image :src="item.cover" mode="aspectFill"/> -->
+								<image src="../../static/home/Swiper.svg" mode="aspectFit"/>
 							</view>
 						</swiper-item>
 					</swiper>
-				</rf-swipe-dot>
+				<!-- </rf-swipe-dot> -->
 			</view>
 		</view>
-
-		<!--&lt;!&ndash;轮播图2&ndash;&gt;-->
-		<!--<swiper class="card-swiper" :circular="true"-->
-		<!--:autoplay="true" interval="5000" duration="500" @change="handleDotChange">-->
-		<!--<swiper-item v-for="(item,index) in carouselList.index_top" :key="index">-->
-		<!--<view class="swiper-item">-->
-		<!--<image :src="item.cover" mode="aspectFill"></image>-->
-		<!--</view>-->
-		<!--<text class="swiper-item-text">{{ `${index + 1}/${carouselList.index_top.length} ${item.title}` }}</text>-->
-		<!--</swiper-item>-->
-		<!--</swiper>-->
+		<view class="swiper_dot" style="height: 20px;">
+		</view>
 		<!-- 分类列表 -->
-		<view class="category-list rf-skeleton">
+		<view class="category-list">
 			<view
-					class="category"
-					v-for="(item, index) in productCateList"
-					:key="index"
-					@tap.prevent="navTo(`/pages/product/list?cate_id=${item.id}`)"
+				class="category"
+				v-for="(item, index) in serveCateList"
+				:key="index"
 			>
-
-				<view class="img">
-					<image :src="item.cover || errorImage" mode="aspectFill"></image>
-				</view>
-				<view class="text">{{ item.title }}</view>
+				<image :src="item.src" mode="aspectFill"></image>
 			</view>
 		</view>
-		<!--新闻通知-->
-		<rf-swiper-slide :list="announceList" class="rf-skeleton">
-			<view slot="header" class="swiper-slide-header">
-				<image class="swiper-slide-image" :src="newsBg"></image>
-			</view>
-		</rf-swiper-slide>
-		<!--新品上市-->
-		<rf-floor-index
-				:list="newProductList"
-				@toBanner="indexTopToDetailPage"
-				@toList="navTo(`/pages/product/list?param=${JSON.stringify({is_new: 1})}`)"
-				:header="{title: '新品上市', desc: 'New Products Listed'}"
-				@detail="navToDetailPage"
-				:banner="carouselList.index_new && carouselList.index_new[0]"/>
-		<!--推荐商品-->
-		<rf-floor-index
-				:list="recommendProductList"
-				:header="{title: '推荐商品', desc: 'Recommend Product'}"
-				@toBanner="indexTopToDetailPage"
-				@toList="navTo(`/pages/product/list?param=${JSON.stringify({is_recommend: 1})}`)"
-				@detail="navToDetailPage"
-				:banner="carouselList.index_recommend && carouselList.index_recommend[0]"/>
-		<!--热门商品-->
-		<rf-floor-index
-				:list="hotProductList"
-				:header="{title: '热门商品', desc: 'Hot Product'}"
-				@toBanner="indexTopToDetailPage"
-				@toList="navTo(`/pages/product/list?param=${JSON.stringify({is_hot: 1})}`)"
-				@detail="navToDetailPage"
-				:banner="carouselList.index_hot && carouselList.index_hot[0]"/>
-		<!--猜你喜欢-->
-		<rf-floor-index
-				:list="guessYouLikeProductList"
-				:header="{title: '猜你喜欢', desc: 'Guess You Like It'}"
-				@toList="navTo(`/pages/product/list?params=${JSON.stringify({guessYouLike: 1})}`)"
-				@detail="navToDetailPage"
-				:bannerShow="false"/>
-		<!--网站备案号-->
-		<!--#ifdef H5-->
-		<view class="copyright" v-if="config.web_site_icp">
-			{{ config.copyright_desc }} <a href='http://www.beian.miit.gov.cn'>{{ config.web_site_icp }}</a>
+		<!--广告-->
+		<view class="ads">
+			<image class="ads_img" src="../../static/home/LeftAdsDiscount.svg"></image>
+			<image class="ads_img" src="../../static/home/RightAdsLocal.svg"></image>
 		</view>
-		<!-- #endif -->
-		<!--页面加载动画-->
-		<rf-loading v-if="loading"></rf-loading>
+		<!--VIP-->
+		<view class="VIP">
+			<image src="../../static/home/MidAdsVip.svg"></image>
+		</view>
+		<!--分享礼品-->
+		<view class="gift_ads">
+			<image class="gift_img" src="../../static/home/LastLeftShare.svg"></image>
+			<image class="gift_img" src="../../static/home/LastRightReturn.svg"></image>
+		</view>
 	</view>
 </template>
 <script>
@@ -123,13 +87,24 @@
                 recommendProductList: [], // 推荐商品列表
                 guessYouLikeProductList: [], // 猜你喜欢商品列表
                 newProductList: [], // 新品上市商品列表
-                productCateList: [],  // 商品栏目
+                serveCateList: [// 服务项目列表
+					{src:"../../static/home/maternity.svg"},
+					{src:"../../static/home/babymeal.svg"},
+					{src:"../../static/home/kids.svg"},
+					{src:"../../static/home/elderly.svg"},
+					{src:"../../static/home/housekeeping.svg"},
+					{src:"../../static/home/hoursworker.svg"},
+					{src:"../../static/home/repairworker.svg"},
+					{src:"../../static/home/painworker.svg"},
+					{src:"../../static/home/moveworker.svg"},
+					{src:"../../static/home/lawnmaintainance.svg"},
+					],  
                 config: {}, // 商户配置
                 announceList: [], // 公告列表
                 loading: true,
                 hotSearchDefault: '请输入关键字',
                 newsBg: this.$mAssetsPath.newsBg,
-                errorImage: this.$mAssetsPath.errorImage
+                // errorImage: this.$mAssetsPath.errorImage
             };
         },
         onShow() {
@@ -294,335 +269,112 @@
     }
 </script>
 <style lang="scss" scoped>
+	page{
+		background-color: #FFFFFF;
+	}
 	.rf-index {
-		background-color: $color-white;
-		/*爆款拼团*/
-		.wrapper {
-			background: #fff;
-			border-radius: 10upx;
-
-			.sec-header {
-				padding: 10upx 0;
-				display: flex;
-				justify-content: space-between;
-				align-items: center;
-				line-height: 40upx;
-				margin: 0 30upx;
-				font-size: $font-lg;
-				font-weight: 600;
-				border-bottom: 1upx solid rgba(0, 0, 0, 0.05);
-
-				.more {
-					font-size: $font-sm;
-					color: $font-color-light;
-					font-weight: normal;
-				}
+		margin-top: 30px;
+		// background-color: #FFFFFF;
+		.header,.location{
+			display: flex;
+			align-items: center;
+		}
+		.header{
+			justify-content: space-between;
+		}
+		// 定位
+		.location{	
+			// margin-top:56px;
+			.location_text{
+				margin-left: 20px;
+				font-family: Tensentype MingSongJ-W2;
+				font-style: Regular;
+				font-size: 12px;
 			}
-
-			.h-list {
-				white-space: nowrap;
-				margin: 20upx 0;
-				height: 290upx;
-
-				.h-item {
-					display: inline-block;
-					font-size: $font-sm;
-					width: 220upx;
-					height: 180upx;
-					margin-right: 20upx;
-					border-radius: 10upx;
-
-					.h-item-img {
-						width: 100%;
-						height: 100%;
-						border-top-left-radius: 12upx;
-						border-top-right-radius: 12upx;
-					}
-
-					.h-item-text {
-						margin: 10upx 0 5upx;
-					}
-
-					.last-line {
-						margin-bottom: 5upx;
-						display: flex;
-						justify-content: space-between;
-						align-items: center; /* 垂直居中 */
-						.red {
-							color: $base-color;
-							font-size: $font-sm;
-							margin-right: 4upx;
-						}
-					}
-
-					.price {
-						font-size: $font-base;
-						color: $base-color;
-						line-height: 1;
-
-						&:before {
-							content: '￥';
-							font-size: 26upx;
-						}
-
-						.m-price {
-							margin-left: 8upx;
-							color: $font-color-light;
-							font-size: $font-sm;
-							text-decoration: line-through;
-						}
-					}
-				}
-
-				image {
-				}
+			.location_img{
+				padding-left: 2px;
+				width: 8px;
+				height: 12px;
 			}
 		}
-
-		.wrapper-pt {
-			padding: 0 30upx;
-
-			.sec-header {
-				margin: 0;
+		.notice{
+			// margin-top:56px;
+			.notice_img{
+				margin-right: 19px;
+				width: 11px;
+				height: 10.77px;
 			}
 		}
-
 		/*轮播图*/
 		.swiper {
-			width: 100%;
-			margin-top: 10upx;
-			display: flex;
-			justify-content: center;
-
+			margin: 10px 20px 0 20px;
+			// height: 224px;
 			.swiper-box {
-				width: 92%;
-				height: 40vw;
-				overflow: hidden;
-				border-radius: 15upx;
-				box-shadow: 0upx 8upx 25upx rgba(0, 0, 0, 0.2);
-				//兼容ios，微信小程序
-				position: relative;
-				z-index: 1;
-
-				swiper {
+				// width: 92%;
+				// height: 40vw;
+				// overflow: hidden;
+				// border-radius: 15upx;
+				// box-shadow: 0upx 8upx 25upx rgba(0, 0, 0, 0.2);
+				// //兼容ios，微信小程序
+				// position: relative;
+				// z-index: 1;
+		
+				swiper {					
 					width: 100%;
-					height: 40vw;
-
+					height: 200px;
 					swiper-item {
+						border-radius: 9px;
 						image {
-							width: 100%;
-							height: 40vw;
+							width: 335px;
+							height: 200px;
+							
 						}
 					}
 				}
 			}
 		}
-
-		/*轮播图2*/
-		.swiper-item-text {
-			position: absolute;
-			bottom: 16upx;
-			left: 30upx;
-			height: 48upx;
-			line-height: 48upx;
-			background: rgba(0, 0, 0, 0.2);
-			width: 90%;
-			color: $color-white;
-			border-bottom-left-radius: 12upx;
-			padding-left: 20upx;
-		}
-
-		/*新闻通知*/
-		.swiper-slide-header {
-			width: 120upx;
-			height: 80upx;
-			line-height: 88upx;
-
-			.swiper-slide-image {
-				width: 100%;
-				height: 30upx;
-			}
-		}
-
 		/*分类列表*/
 		.category-list {
-			width: 100%;
-			padding: $spacing-base;
+			height: 150px;
+			margin: 0 20px ;
 			display: flex;
-			justify-content: space-between;
 			flex-wrap: wrap;
-			min-height: 80upx;
+			background-color: #ffe8cd;
+			border-radius: 9px;
 			.category {
-				width: calc(20% - 20upx);
 				display: flex;
 				flex-wrap: wrap;
-				.img {
-					width: 100%;
-					display: flex;
-					justify-content: center;
 					image {
-						width: 9vw;
-						height: 9vw;
+						margin: 8px 7px;
+						width: 50px;
+						height: 54px;
 					}
-				}
-				.text {
-					margin-top: 16upx;
-					width: 100%;
-					display: flex;
-					justify-content: center;
-					font-size: 24upx;
-					color: #3c3c3c;
-				}
 			}
 		}
-
-		/*版权显示*/
-		.copyright {
-			margin: 10upx 0;
-			width: 100%;
-			text-align: center;
-			color: #666;
-
-			a {
-				display: block;
-				color: #666;
-				text-decoration: none;
-			}
-		}
-
-		/*限时抢购*/
-		.order-item {
+		.ads{
+			margin: 10px 20px;
 			display: flex;
-			flex-direction: column;
-			background: #fff;
-			padding: 0 30upx;
-			margin-bottom: 20upx;
-
-			.goods-box-single {
-				display: flex;
-				padding: 40upx 10upx 10upx;
-				height: 330upx;
-				border-radius: 12upx;
-				margin-top: 20upx;
-				box-shadow: 2px 2px 10px rgba(66, 135, 193, 0.2); // 阴影
-				border-bottom: 1px solid rgba(0, 0, 0, 0.05); // 边框
-				position: relative;
-
-				.goods-img {
-					display: block;
-					border-radius: 12upx;
-					width: 190upx;
-					height: 200upx;
-				}
-
-				.right {
-					flex: 1;
-					display: flex;
-					flex-direction: column;
-					padding: 0 30upx 0 24upx;
-					overflow: hidden;
-
-					.title {
-						font-weight: bold;
-						line-height: 1.2;
-						margin: 10upx 0;
-					}
-
-					.attr-box {
-						line-height: 1.2;
-						margin-bottom: 8upx;
-						margin-left: 10upx;
-					}
-
-					.last-line {
-						margin-top: 3upx;
-						display: flex;
-						justify-content: space-between;
-						align-items: center; /* 垂直居中 */
-						.red {
-							color: $base-color;
-							margin-right: 4upx;
-						}
-					}
-
-					.price {
-						font-size: $font-lg;
-						color: $uni-color-error;
-						line-height: 1;
-
-						&:before {
-							content: '￥';
-							font-size: 26upx;
-						}
-
-						.m-price {
-							margin-left: 8upx;
-							color: $font-color-light;
-							font-size: $font-sm;
-							text-decoration: line-through;
-						}
-					}
-
-					.triangle-wrapper {
-						position: absolute;
-						overflow: hidden;
-						top: 0;
-						right: 0;
-						border-radius: 12upx;
-
-						.triangle {
-							color: #5eba8f;
-							width: 0;
-							height: 0;
-							border-top: 120upx solid $base-color;
-							opacity: 0.8;
-							border-left: 120upx solid transparent;
-						}
-
-						.triangle-content {
-							position: absolute;
-							top: 28upx;
-							right: 0;
-							transform: rotate(45deg);
-							font-size: $font-sm - 2upx;
-							color: #fff;
-						}
-					}
-				}
-			}
-
-			.action-box {
-				display: flex;
-				justify-content: flex-end;
-				align-items: center;
-				height: 90upx;
-				position: relative;
-
-				.discount-time {
-					font-size: $font-sm;
-					color: $uni-color-success;
-					margin-right: 20upx;
-				}
-			}
-
-			.action-btn {
-				height: 50upx;
-				margin-left: 10upx;
-				display: flex;
-				justify-content: center;
-				align-items: center;
-				padding: 10upx 24upx;
-				font-size: $font-sm + 2upx;
-
-				&.recom {
-					background: $base-color;
-					color: #fff;
-
-					&:after {
-						border-color: #f7bcc8;
-					}
-				}
+			.ads_img{
+				// width: 162.5px;
+				height: 90px;
 			}
 		}
+		.VIP{	
+			margin: 0px 20px;
+			height: 70px;
+			image{
+				position: relative;
+				height: 70px;								
+			}
+		}
+		.gift_ads{
+			margin: 10px 20px 13px 20px;
+			display: flex;
+			.gift_img{
+				// width: 162.5px;
+				height: 70px;
+			}
+		}
+		
 	}
 </style>
