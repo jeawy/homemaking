ee9c4bab8cfc745870131a5a900bb9e6ef881f8f
zzz19680410 <1737403035@qq.com>
2021-01-04 21:28:30 +0800

    首页的定位搜索弹框

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -3,7 +3,7 @@
 		<!-- 顶部信息 -->
 		<view class="header">
 			<!--位置信息-->
-			<view class="location">
+			<view class="location" @tap="positionSearch">
 				<view class="location_text">西安</view>
 				<image class="location_img" src="../../static/home/City.svg"></image>		
 			</view>
@@ -101,14 +101,38 @@
 						height: 28rpx;" class="bottom_img" src="../../static/home/correct.svg"></image>
 				</view>
 			</view>
-		</scroll-view>		
+		</scroll-view>	
+		<s-popup custom-class="demo-popup" position="center" v-model="cityVisiable">
+			<view class="position_search">				
+				<view class="position_box">
+					<view class="position_title">请选择服务地址</view>
+					<view class="titlename">当前城市</view>
+					<view class="box_image">
+						<view class="box_title">
+							<view class="box_name">悉尼</view>
+							<image class="position_img" src="../../static/home/Position.svg"></image>
+						</view>
+						<image class="refresh_img" src="../../static/home/Refresh.svg"></image>
+					</view>
+				</view>
+				<view class="value">
+					<view class="value_name">热门城市</view>
+					<view class="cityList">
+						<view class="city_text" v-for="(item,index) in citylist" 
+						:key="index">{{item.value}}</view>
+					</view>
+				</view>
+			</view>
+		</s-popup>
 	</view>
 </template>
 <script>
 	import mainCard from '@/components/main-card.vue';
+	import sPopup from '@/components/s-popup/index.vue';
     export default {
         data() {
             return {
+				cityVisiable:false,
 				swiperImg: [// 轮播图图片
 					'../../static/home/Swiper.svg',
 					'../../static/home/Swiper.svg',
@@ -134,6 +158,20 @@
 					language:['普通话','英语'],
 					type:"包月小时工"
 				}],
+				citylist:[
+					{value:'悉尼'},
+					{value:'墨尔本'},
+					{value:'布里斯班'},
+					{value:'柏斯'},
+					{value:'阿德莱德'},
+					{value:'黄金海岸'},
+					{value:'卧龙岗'},
+					{value:'堪培拉'},
+					{value:'基隆'},
+					{value:'霍巴德'},
+					{value:'达尔文'},
+					{value:'洛坎普顿'},		
+					]
                 // config: {}, // 商户配置
                 // announceList: [], // 公告列表
                 // loading: true,
@@ -143,7 +181,8 @@
             };
         },
         components: {
-        	mainCard
+        	mainCard,
+			sPopup
         },
 		onShow() {
             // this.initData();
@@ -184,6 +223,10 @@
             this.getIndexList('refresh');
         },
         methods: {
+			// 定位搜索
+			positionSearch(){
+				this.cityVisiable = !this.cityVisiable
+			},
 			// 页面跳转（查看更多）
 			target(url){
 				uni.switchTab({
@@ -332,7 +375,9 @@
 			padding-top: 56rpx;
 			justify-content: space-between;
 		}
-		.fonttext,.location_text,.search_more,.assure_text{
+		.fonttext,.location_text,.search_more,.assure_text
+		,.position_title,.titlename,.value_name,.box_name,
+		.city_text{
 			font-style: normal;
 			font-weight: 400;
 			letter-spacing: 0em;
@@ -551,6 +596,99 @@
 				.bottom_img{
 					width: 28rpx;
 					height: 28rpx;
+				}
+			}
+		}
+		.demo-popup{
+			.s-popup-wrap{
+				height: 316rpx;
+			}
+		}
+		.position_search{
+			width: 560rpx;		
+			.box_image{
+				display: flex;
+				align-items: center;
+			}
+			.position_box{
+				// height: 114rpx;
+				.position_title{
+					text-align: center;
+					margin-top: 10rpx;
+					margin-bottom: 8rpx;
+					font-family: Tensentype MingSongJ-W2;
+					font-size: 20rpx;
+					line-height: 24rpx;				
+				}
+				.titlename{
+					font-family: Tensentype MingSongJ-W2;
+					font-size: 18rpx;
+					line-height: 22rpx;
+					margin-left: 10rpx;
+				}
+			}
+			.box_image{	
+				justify-content: space-between;
+				margin-top: 8rpx;
+				margin-bottom: 20rpx;
+				.box_title{
+					display: flex;
+					margin-left: 20rpx ;					
+					align-items: center;
+					.box_name{
+						font-family: Tensentype MingSongJ-W8;
+						font-size: 18rpx;
+						// font-style: normal;
+						font-weight: 900;
+						line-height: 22rpx;
+						// letter-spacing: 0em;						
+					}
+					.position_img{
+						width: 9.17px;
+						height: 9.17px;
+						margin-left: 3.42px;					
+					}					
+				}
+				.refresh_img{
+					width: 8.27px;
+					height: 8.26px;
+					margin-right: 11.36px;
+				}
+			}						
+			.value{
+				background: #fff7ee;
+				height: 202rpx;
+				border-radius: 8rpx;
+				.value_name{
+					font-family: Tensentype MingSongJ-W2;
+					font-size: 18rpx;
+					line-height: 22rpx;
+					margin: 0 0 20rpx 10rpx;
+					padding-top: 6rpx;
+				}
+				.cityList{
+					display: flex;
+					flex-wrap: wrap;
+					align-items: center;
+					.city_text{
+						display: flex;
+						align-items: center;
+						justify-content: center;
+						width: 100rpx;
+						height: 32rpx;
+						background: #FDB667;
+						border-radius: 4rpx;
+						margin: 0 20rpx 20rpx 20rpx;
+						font-family: Tensentype MingSongJ-W4;
+						font-size: 18rpx;
+						// font-style: normal;
+						// font-weight: 400;
+						line-height: 22rpx;
+						// letter-spacing: 0em;
+						text-align: center;
+						color: #ffffff;
+
+					}
 				}
 			}
 		}
