064a38cf264732503805139117e69c9baae0a077
zzz19680410 <1737403035@qq.com>
2021-01-14 16:50:43 +0800

    修改后端地址

--- config/index.config.js
+++ config/index.config.js
@@ -2,7 +2,8 @@ const CONFIG = {
     //开发环境配置
     development: {
         assetsPath: '/static', // 静态资源路径
-        baseUrl: 'http://shop.map2family.com/api',  // 后台接口请求地址
+        // baseUrl: 'http://shop.map2family.com/api',  // 后台接口请求地址
+        baseUrl: 'http://47.95.239.228:8091/api',  // 后台接口请求地址
         hostUrl: 'http://localhost:8080',        // H5地址(前端运行地址)
         websocketUrl: '',        // websocket服务端地址
         weixinAppId: '' // 微信公众号appid
@@ -10,8 +11,8 @@ const CONFIG = {
     //生产环境配置
     production: {
         assetsPath: '/static', // 静态资源路径
-        baseUrl: 'http://shop.map2family.com/api',  // 后台接口请求地址
-        hostUrl: 'http://ws.map2family.com',        // H5地址(前端运行地址)
+        baseUrl: 'http://47.95.239.228:8091/api',  // 后台接口请求地址
+        hostUrl: 'http://localhost:8080',        // H5地址(前端运行地址)
         websocketUrl: '',        // websocket服务端地址
         weixinAppId: '' // 微信公众号appid
     }
